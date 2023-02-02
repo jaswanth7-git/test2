c0d0ead304f6081f28de1f107329dba03175e291
Jaswanth Alla <79307217+jaswanth7-git@users.noreply.github.com>
2023-02-02 11:17:45 +0530

    Add files via upload

---
+++ second/db.sql
@@ -0,0 +1,10 @@
+create table emp(
+	empcode int,
+	name varchar(50),
+	designation varchar(50),
+	email varchar(50), 
+	location varchar(50)
+);
+
+insert into emp values(1,'jaswanth','Manager','jaswanth.alla@bajajfinserv.in','Pune'),
+(2,'dheeraj','SDE','p.dheeraj@bajajfinserv.in','Delhi'),(3,'ramarao','CEO','ram.rao@gmail.com','Vizag');
